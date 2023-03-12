5f98971ee25c302e93991f86fc5faa7e97ec0e4e
munyite001 <emunyite@gmail.com>
2023-03-12 17:29:38 +0300

    Add snake head

--- app.js
+++ app.js
@@ -2,11 +2,47 @@ const playBoard = document.querySelector('.play-board');
 
 let foodX, foodY;
 
+let snakeX = 5, snakeY = 10;
+let velocityX = 0, velocityY = 0;
+
+const changeDirection = (e) => {
+//  Changing velocity value based on keypress
+console.log(e.key)
+    if (e.key === "ArrowUp")
+    {
+        velocityY = 0;
+        velocityX = -1;
+    }
+    else if(e.key === "ArrowDown")
+    {
+        velocityY = 0;
+        velocityX = 1;
+    }
+    else if(e.key === "ArrowRight")
+    {
+        velocityY = 1;
+        velocityX = 0;
+    }
+    else if(e.key == "ArrowLeft")
+    {
+        velocityY = -1;
+        velocityX = 0;
+    }
+
+    initGame();
+}
+
 //  Function to initialize the game
 //  It creates a food for the snake and displays it
 const initGame = () => {
-    let snakeFood = `<div class="food" style="grid-area: ${foodX}/${foodY}"></div> `;
-    playBoard.innerHTML = snakeFood;
+    let htmlMarkup = `<div class="food" style="grid-area: ${foodX}/${foodY}"></div> `;
+    htmlMarkup += `<div class="snakeHead" style="grid-area: ${snakeX}/${snakeY}"></div> `;
+
+    // updating the snake head velocity, based on current velocity
+    snakeX += velocityX;
+    snakeY += velocityY;
+
+    playBoard.innerHTML = htmlMarkup;
 }
 
 //  Function to randomize the snake food
@@ -16,4 +52,5 @@ const randomFoodPosition = () => {
 }
 
 randomFoodPosition();
-initGame();
+setInterval(initGame, 125);
+document.addEventListener('keydown', changeDirection);
