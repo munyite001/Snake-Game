21dd73da819723e371ddd5063a6e358ad782a14c
munyite001 <emunyite@gmail.com>
2023-03-13 21:47:02 +0300

    add growth functionality to snake

--- app.js
+++ app.js
@@ -1,55 +1,67 @@
-const playBoard = document.querySelector('.play-board'); 
+const playBoard = document.querySelector('.play-board');
 
 let foodX, foodY;
 
-let snakeX = 5, snakeY = 10;
-let velocityX = 0, velocityY = 0;
+let snakeX = 15; let snakeY = 15;
+let snakeBody = [];
+let velocityX = 0; let velocityY = 0;
 
 const changeDirection = (e) => {
 //  Changing velocity value based on keypress
-console.log(e.key)
-    if (e.key === "ArrowUp")
-    {
-        velocityY = 0;
-        velocityX = -1;
-    }
-    else if(e.key === "ArrowDown")
-    {
-        velocityY = 0;
-        velocityX = 1;
-    }
-    else if(e.key === "ArrowRight")
-    {
-        velocityY = 1;
-        velocityX = 0;
-    }
-    else if(e.key == "ArrowLeft")
-    {
-        velocityY = -1;
-        velocityX = 0;
-    }
+  if (e.key === 'ArrowUp') {
+    velocityY = 0;
+    velocityX = -1;
+  } else if (e.key === 'ArrowDown') {
+    velocityY = 0;
+    velocityX = 1;
+  } else if (e.key === 'ArrowRight') {
+    velocityY = 1;
+    velocityX = 0;
+  } else if (e.key === 'ArrowLeft') {
+    velocityY = -1;
+    velocityX = 0;
+  }
 
-    initGame();
-}
+  initGame();
+};
 
 //  Function to initialize the game
 //  It creates a food for the snake and displays it
 const initGame = () => {
-    let htmlMarkup = `<div class="food" style="grid-area: ${foodX}/${foodY}"></div> `;
-    htmlMarkup += `<div class="snakeHead" style="grid-area: ${snakeX}/${snakeY}"></div> `;
+  let htmlMarkup = `<div class="food" style="grid-area: ${foodX}/${foodY}"></div> `;
 
-    // updating the snake head velocity, based on current velocity
-    snakeX += velocityX;
-    snakeY += velocityY;
+  //  Checking if the snake hit the food
+  if (snakeX === foodX && snakeY === foodY) {
+    randomFoodPosition();
+    snakeBody.push([foodX, foodY]); //  Pushing food position to snake body array
+    console.log(snakeBody);
+  }
 
-    playBoard.innerHTML = htmlMarkup;
-}
+  // updating the snake head velocity, based on current velocity
+  snakeX += velocityX;
+  snakeY += velocityY;
+
+  //  Shifting forward the snake values plus one
+  for (let i = snakeBody.length - 1; i > 0; i--) {
+    snakeBody[i] = snakeBody[i - 1];
+    
+  }
+
+  snakeBody[0] = [snakeX, snakeY];  //  Setting first element of snake body to be current snake position
+
+  for (let i = 0; i < snakeBody.length; i++) {
+    //  Adding a div for each part of the snake's body
+    htmlMarkup += `<div class="snakeHead" style="grid-area: ${snakeBody[i][0]}/${snakeBody[i][1]}"></div> `;
+  }
+
+  playBoard.innerHTML = htmlMarkup;
+};
 
 //  Function to randomize the snake food
 const randomFoodPosition = () => {
-    foodX = Math.floor(Math.random() * 30) + 1;
-    foodY = Math.floor(Math.random() * 30) + 1;
-}
+  foodX = Math.floor(Math.random() * 30) + 1;
+  foodY = Math.floor(Math.random() * 30) + 1;
+};
 
 randomFoodPosition();
 setInterval(initGame, 125);
